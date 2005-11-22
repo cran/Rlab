acf84e40810473001c91f7e74b3f7ae8192d0cbc
Atina Dunlap Brooks <adbrook2@stat.ncsu.edu>
2005-11-22 00:00:00 +0000

    version 1.2

--- R/bplot.R
+++ R/bplot.R
@@ -1,16 +1,12 @@
-"bplot" <-
-function (x, by, style = "tukey", outlier = TRUE, plot = TRUE, 
-    ...) 
-
-{
-
-    obj <- stats.bplot(x, style = style, outlier = outlier, by = by)
-    if (plot) {
-        bplot.obj(obj, ...)
-    }
-    else {
-        return(obj)
-    }
-    invisible()
-
-}
+"bplot" <-
+function (x, by,style = "tukey", outlier = TRUE, plot = TRUE, ...) 
+{
+    obj <- stats.bplot(x, style = style, outlier = outlier, by=by)
+    if (plot) {
+        bplot.obj(obj, ...)
+    }
+    else {
+        return(obj)
+    }
+    invisible()
+}
