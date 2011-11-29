5a36b720c2b9d04ae6cc86399d22553170e656a9
Dennis Boos <boos@stat.ncsu.edu>
2011-11-29 00:00:00 +0000

    version 2.14.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,4 @@
 ".First.lib" <-
 function (lib, pkg) 
 {data(list=data(package=pkg)$results[,3],envir=as.environment(match(paste("package",pkg,sep=":"), search())))
-cat(pkg,"functions and datasets have been loaded.  Use help(",pkg,") for an overview of this library", 
-fill=T)
 }
